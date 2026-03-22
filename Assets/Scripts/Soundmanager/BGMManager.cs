aef0c23dd8adf208bae978958eb9a53c9b817fbb
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2026-03-23 02:11:27 +0900

    Update: Deleted CRIWARE to fix 音声認識

--- Assets/Scripts/Soundmanager/BGMManager.cs
+++ Assets/Scripts/Soundmanager/BGMManager.cs
@@ -1,19 +1,17 @@
-using CriWare;
-
 public class BGMManager : Singleton<BGMManager>
 {
     // public string cueName;
-    private CriAtomSource _criAtomSource;
+    // private CriAtomSource _criAtomSource;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        DontDestroyOnLoad(gameObject);
-    }
+    // protected override void Awake()
+    // {
+    //     base.Awake();
+    //     DontDestroyOnLoad(gameObject);
+    // }
 
-    void Start()
-    {
-        _criAtomSource = GetComponent<CriAtomSource>();
-        _criAtomSource.Play();
-    }
+    // void Start()
+    // {
+    //     _criAtomSource = GetComponent<CriAtomSource>();
+    //     _criAtomSource.Play();
+    // }
 }

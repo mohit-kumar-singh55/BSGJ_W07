e297215ba4d7b237b4b4e2737e8ffbf5a1128bf2
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2026-04-21 02:23:07 +0900

    Update: updated lightning & post build

--- Assets/Scripts/Gameplay/Customer System/States/CustomerInComing.cs
+++ Assets/Scripts/Gameplay/Customer System/States/CustomerInComing.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using UnityEngine.AI;
 
 /// <summary>
 /// Moves the customer to the seat
@@ -25,7 +25,15 @@ public class CustomerInComing : BaseState<Customer.CustomerState>
     public override Customer.CustomerState GetNextState()
     {
         // _canTransition = Mathf.Approximately(_context.CustomerAgent.remainingDistance, 0);
-        _canTransition = _context.CustomerAgent.remainingDistance <= _context.CustomerAgent.stoppingDistance;
+        // _canTransition = _context.CustomerAgent.remainingDistance <= _context.CustomerAgent.stoppingDistance;
+        // _canTransition = !_context.CustomerAgent.pathPending;
+        NavMeshAgent agent = _context.CustomerAgent;
+
+        bool reached = !agent.pathPending &&
+            agent.remainingDistance <= agent.stoppingDistance &&
+            (!agent.hasPath || agent.velocity.sqrMagnitude < 0.01f);
+
+        _canTransition = reached;
 
         // set final rotation towards the table
         if (_canTransition) _context.ThisCustomer.transform.rotation = _context.CustomerStandPoint.rotation;

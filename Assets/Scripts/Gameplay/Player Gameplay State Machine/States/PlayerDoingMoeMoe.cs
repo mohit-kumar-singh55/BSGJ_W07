683b12ab592f666d3449fa75cfd9586caf4dd394
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2026-05-08 11:57:42 +0900

    Update: minor change

--- Assets/Scripts/Gameplay/Player Gameplay State Machine/States/PlayerDoingMoeMoe.cs
+++ Assets/Scripts/Gameplay/Player Gameplay State Machine/States/PlayerDoingMoeMoe.cs
@@ -13,7 +13,6 @@ public class PlayerDoingMoeMoe : BaseState<PlayerStateManager.PlayerState>
     private const string KYUN_ANIM = "Kyun";
     private const string HEART_PULSE = "Pulse";
 
-    public static event System.Action OnPlayerEnteredMoeMoe = delegate { };
     public static event System.Action OnMoeMoeStarted = delegate { };
     public static event System.Action OnMoeMoeCompleted = delegate { };
 
@@ -50,8 +49,6 @@ public class PlayerDoingMoeMoe : BaseState<PlayerStateManager.PlayerState>
             // moe moe started
             OnMoeMoeStarted?.Invoke();
         });
-
-        OnPlayerEnteredMoeMoe?.Invoke();
     }
 
     public override void UpdateState() { }
